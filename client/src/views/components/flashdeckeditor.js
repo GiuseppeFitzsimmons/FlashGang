782d5e1a5d61e2a1b0e4457ccd008d5db71df9ff
Phillip Fitzsimmons <phillip.fitzsimmons@gmail.com>
2019-10-07 21:33:19 +0200

    Grid applied to FlashEditor.js

--- client/src/views/components/flashdeckeditor.js
+++ client/src/views/components/flashdeckeditor.js
@@ -4,85 +4,80 @@ import { connect } from 'react-redux'
 import { bindActionCreators } from 'redux'
 import * as Actions from '../../action'
 import IntegratedInput from '../widgets/IntegratedInput'
-import {
-  Button,
-  Col,
-  Input
-} from "reactstrap";
+import { Button, Grid, GridList } from '@material-ui/core';
 import { makeStyles } from '@material-ui/core/styles';
 
 class FlashDeckEditor extends React.Component {
-    render() {
-        const flashDeck = this.props.flashDeck
-      return (
-        <>
-        <Col>
+  render() {
+    const flashDeck = this.props.flashDeck
+    return (
+      <Grid container
+          direction="column"
+          justify="space-between"
+          alignItems="stretch"
+          >
           <IntegratedInput
             label="FlashDeck Name"
             id='flashCardName'
             placeholder='flash card name'
             onChange={
-                (event) => { flashDeck.name = event.target.value }
+              (event) => { flashDeck.name = event.target.value }
             }
             ref={
-                input=>input ? input.reset(flashDeck.name) : true
+              input => input ? input.reset(flashDeck.name) : true
             }
           />
-        </Col>
-        <Col>
           <IntegratedInput
             label="Description"
             id='flashCardDescription'
             placeholder='flash card description'
             onChange={
-                (event) => { flashDeck.description = event.target.value }
+              (event) => { flashDeck.description = event.target.value }
             }
             ref={
-                input=>input ? input.reset(flashDeck.description) : true
+              input => input ? input.reset(flashDeck.description) : true
             }
           />
-          </Col>
-        <Col>
           <IntegratedInput
             label="Fuzziness"
             type='number'
             id='flashCardFuzziness'
             placeholder='flash card fuzziness'
             onChange={
-                (event) => { flashDeck.fuzziness = event.target.value }
+              (event) => { flashDeck.fuzziness = event.target.value }
             }
             ref={
-                input=>input ? input.reset(flashDeck.fuzziness) : true
+              input => input ? input.reset(flashDeck.fuzziness) : true
             }
           />
-          </Col>
-        <Col>
-          <AwesomeButton
-            onPress = {() => this.props.nextCard(flashDeck)}
+          <Button
+            color='primary'
+            variant='contained'
+            onClick={
+              () => this.props.nextCard(flashDeck)
+            }
           >
-              Edit Cards
-          </AwesomeButton>
-          </Col>
-        <Col>
-          <AwesomeButton
-                  onPress={() => {
-                        this.props.saveDeck(this.props.flashDeck)
-                    }
-                  }
-              >
-              Save Deck
-            </AwesomeButton>
-          </Col>
-        </>
-      )
-    }
+            Edit Cards
+          </Button>
+          <Button
+            color='primary'
+            variant='contained'
+            onClick={() => {
+              this.props.saveDeck(this.props.flashDeck)
+            }}
+          >
+            Save Deck
+          </Button>
+      </Grid>
+    )
   }
+}
 
 function mapStateToProps(state, props) {
-    return {}
-  }
-  function mapDispatchToProps(dispatch) {
-    return bindActionCreators(Actions, dispatch)
-  }
-  
+  return {}
+}
+function mapDispatchToProps(dispatch) {
+  return bindActionCreators(Actions, dispatch)
+}
+
 export default connect(mapStateToProps, mapDispatchToProps)(FlashDeckEditor)
