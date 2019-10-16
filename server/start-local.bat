fef6c41195534e70589ca4b2260e0167282a1fc1
Phillip Fitzsimmons <phillip.fitzsimmons@gmail.com>
2019-10-16 21:21:56 +0200

    start local

--- server/start-local.bat
+++ server/start-local.bat
@@ -1,3 +1,2 @@
 start java -Djava.library.path=../dynamoDB/DynamoDBLocal_lib -jar ../dynamoDB/DynamoDBLocal.jar -sharedDb
-node crock/index --parameter-file "../local-environment-vars.json" --db-endpoint http://localhost:8000 --db-batch-write-file "../dynamoDB/mockusers.json"
-REM node crock/index --parameter-file "../local-environment-vars.json" --db-endpoint "http://localhost:8000" --db-retain
+crockstack --env-vars "devvariables.json" 
