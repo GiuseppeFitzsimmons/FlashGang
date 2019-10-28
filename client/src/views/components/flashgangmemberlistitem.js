de7995ebd91a718685388d201a3abc7b827cf9b3
Phillip Fitzsimmons <philip.fitzsimmons.acensisana@axa.com>
2019-10-28 17:50:04 +0100

    nicer member list item

--- client/src/views/components/flashgangmemberlistitem.js
+++ client/src/views/components/flashgangmemberlistitem.js
@@ -26,7 +26,10 @@ const randomProfiles = [
     'https://upload.wikimedia.org/wikipedia/commons/f/f1/Sophia_Loren_1962.jpg',
     'https://upload.wikimedia.org/wikipedia/commons/1/1b/LuckyLucianoSmaller.jpeg',
     'https://i.ebayimg.com/images/g/NIAAAOSwAaJaQuGk/s-l300.jpg',
-    'https://upload.wikimedia.org/wikipedia/commons/thumb/6/62/Carlo_Gambino.jpg/1280px-Carlo_Gambino.jpg'
+    'https://upload.wikimedia.org/wikipedia/commons/thumb/6/62/Carlo_Gambino.jpg/1280px-Carlo_Gambino.jpg',
+    'https://www.onthisday.com/images/people/bugsy-siegel-medium.jpg',
+    'https://themobmuseum.org/wp-content/uploads/2016/03/3-21-16-Virginia-Hill.jpg',
+
 ]
 
 class FlashGangMemberListItemStyled extends React.Component {
@@ -34,176 +37,6 @@ class FlashGangMemberListItemStyled extends React.Component {
         super(props)
         this.state = { editing: false, rank: 'MEMBER' }
     }
-    renderOld() {
-        var editLevel = 2;
-        if (this.props.flashGang) {
-            if (this.props.flashGang.rank == 'LIEUTENANT') {
-                editLevel = 1;
-            } else if (this.props.flashGang.rank == 'BOSS') {
-                editLevel = 0;
-            }
-        }
-        return (
-            <Grid>
-                <div style={{
-                    position: 'relative',
-                    top: 0, left: 0,
-                    display: editLevel < 2 && this.props.gangMember.rank != 'BOSS' ? '' : 'none', zIndex: 99,
-                    bottomMargin: '18px'
-                }}>
-                    <div style={{
-                        position: 'absolute',
-                        top: 0, left: 0,
-                        ...this.props.theme.systemButton
-                    }}>
-                        <MdModeEdit
-                            onClick={
-                                () => {
-                                    this.setState({ editing: !this.state.editing })
-                                }
-                            }
-                        />
-                        <MdDelete
-                            style={{ display: this.state.editing ? '' : 'none' }}
-                            onClick={
-                                () => {
-                                    this.props.onDelete()
-                                }
-                            }
-                        />
-
-                    </div>
-                </div>
-                <ListItem
-                    alignItems="flex-start"
-                    button
-
-                >
-                    <Grid
-                        container
-                        direction='row'
-                        alignItems='flex-top'
-                    >
-                        <Grid
-                            item
-                            xs='2'
-                        >
-
-                            <ListItemAvatar
-                                backgroundColor='blue'
-                            >
-                                <Icon style={{ fontSize: 30 }}>{this.props.gangMember.icon}</Icon>
-                            </ListItemAvatar>
-                        </Grid>
-                        <Grid
-                            item
-                            xs='9'
-                        >
-                            <Grid
-                                container
-                                direction='row'
-                                alignItems='flex-end'
-                                justify='space-between'
-                            >
-                                <Grid
-                                    item
-                                    xs='11'
-                                >
-                                    <div
-                                        style={{
-                                            display: !this.state.editing ? 'block' : 'none'
-                                        }}
-                                    >
-                                        {
-                                            !this.props.gangMember.firstName && !this.props.gangMember.lastName ?
-                                                this.props.gangMember.id :
-                                                this.props.gangMember.firstName ? this.props.gangMember.firstName : '' +
-                                                    this.props.gangMember.lastName ? this.props.gangMember.lastName : ''
-                                        }
-                                    </div>
-
-                                    <div
-                                        style={{
-                                            display: this.state.editing ? 'block' : 'none'
-                                        }}
-                                    >
-                                        <IntegratedInput
-                                            id='id'
-                                            placeholder='Gang member email'
-                                            onChange={
-                                                (event) => { this.props.gangMember.id = event.target.value }
-                                            }
-                                            ref={
-                                                input => input ? input.reset(this.props.gangMember.id) : true
-                                            }
-                                        />
-                                    </div>
-                                </Grid>
-                                <Grid
-                                    item
-                                    xs='1'
-                                >
-                                    <div
-                                        style={{
-                                            display: !this.state.editing ? 'block' : 'none'
-                                        }}
-                                    >
-                                        {
-                                            this.props.gangMember.rank == 'BOSS' &&
-                                            <GiBrutalHelm style={this.props.theme.icon} />
-                                        }
-                                        {
-                                            this.props.gangMember.rank == 'LIEUTENANT' &&
-                                            <GiSwordman style={this.props.theme.icon} />
-                                        }
-                                        {
-                                            this.props.gangMember.rank == 'MEMBER' &&
-                                            <GiHoodedFigure style={this.props.theme.icon} />
-                                        }
-                                    </div>
-                                    <div
-                                        style={{
-                                            display:
-                                                this.state.editing &&
-                                                    editLevel < 2 &&
-                                                    (this.props.gangMember.rank != 'BOSS' || this.props.flashGang.rank == 'BOSS')
-                                                    ? 'block' : 'none'
-                                        }}
-                                    >
-                                        <Select
-                                            value={this.props.gangMember.rank}
-                                            onChange={
-                                                (event) => {
-                                                    this.props.gangMember.rank = event.target.value;
-                                                    this.forceUpdate()
-                                                }
-                                            }
-                                        >
-                                            {editLevel < 1 &&
-                                                <MenuItem value={'BOSS'}>
-                                                    <GiBrutalHelm style={this.props.theme.icon} />
-                                                </MenuItem>
-                                            }
-                                            {editLevel < 2 &&
-                                                <MenuItem value={'LIEUTENANT'}>
-                                                    <GiSwordman style={this.props.theme.icon} />
-                                                </MenuItem>
-                                            }
-                                            <MenuItem value={'MEMBER'}>
-                                                <GiHoodedFigure style={this.props.theme.icon} />
-                                            </MenuItem>
-                                        </Select>
-
-                                    </div>
-                                </Grid>
-                            </Grid>
-                        </Grid>
-                    </Grid>
-                </ListItem>
-            </Grid>
-        )
-    }
-
     render() {
         var editLevel = 2;
         if (this.props.flashGang) {
@@ -250,14 +83,15 @@ class FlashGangMemberListItemStyled extends React.Component {
                     </div>
                     <Container style={{
                         height: '100%',
-                        background: `url('${this.props.gangMember.profileImage}')`,
-                        backgroundSize: '60px',
+                        backgroundImage: `url('${this.props.gangMember.profileImage}')`,
+                        backgroundSize: '100%',
                         backgroundRepeat: 'no-repeat'
-                    }}>
+                    }}
+                    height={'10%'}>
                     </Container>
                 </Grid>
                 <Grid item xs={8} sm={8} md={8} style={this.props.theme.actionListItem}>
-                    <Container style={{ height: 60, verticalAlign: 'middle' }}>
+                    <Container height={'10%'} style={{ verticalAlign: 'middle' }}>
 
                         <div
                             style={{
