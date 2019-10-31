abb976af6f05fe74072bd8ac94328973f055fbb3
giuseppefitzsimmons <giuseppefitzsimmons@gmail.com>
2019-10-31 18:12:39 +0100

    scripts (untried) for deploying the front

--- deploy.sh
+++ deploy.sh
@@ -2,3 +2,5 @@ npm run install:linux --prefix server/commonlayer/nodejs/
 
 sam package --template-file server/template.yaml --output-template-file packaged.yaml --profile phillip --s3-bucket wwdd-build-bucket-us-east-1
 sam deploy --template-file packaged.yaml --stack-name flashgang-dev  --profile phillip --capabilities CAPABILITY_NAMED_IAM --region us-east-1 --parameter-overrides 'DynamoDbEndpoint='
+
+./client/deploy.sh
