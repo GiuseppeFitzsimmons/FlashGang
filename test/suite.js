628c8d9e12ec175cb751f922f49b594b14387393
Phillip Fitzsimmons <phillip.fitzsimmons@gmail.com>
2020-01-03 14:41:19 +0100

    twelve new accounts from the Sopranos

--- test/suite.js
+++ test/suite.js
@@ -3,17 +3,98 @@ const fetch=require('node-fetch')
 const domain='http://localhost:8080';
 
 const createAccountTony = {
-    id: 'tony@soprano.id',
+    id: 'tony@soprano.it',
     firstName: 'Tony',
     lastName: 'Soprano',
     password: 'password'
 }
 const createAccountChris = {
-    id: 'chris@soprano.id',
+    id: 'chris@soprano.it',
     firstName: 'Chris',
+    lastName: 'Moltisanti',
+    password: 'password'
+}
+const createAccountCarmella = {
+    id: 'carmella@soprano.it',
+    firstName: 'Carmella',
     lastName: 'Soprano',
     password: 'password'
 }
+const createAccountJunior = {
+    id: 'junior@soprano.it',
+    firstName: 'Junior',
+    lastName: 'Soprano',
+    password: 'password'
+}
+const createAccountPaulie = {
+    id: 'paulie@soprano.it',
+    firstName: 'Paulie',
+    lastName: 'Gualtieri',
+    nickName: 'Wallnuts',
+    password: 'password'
+}
+const createAccountBobby = {
+    id: 'bobby@soprano.it',
+    firstName: 'Bobby',
+    lastName: 'Baccalieri',
+    nickName: 'Bacala',
+    password: 'password'
+}
+const createAccountVito = {
+    id: 'vito@soprano.it',
+    firstName: 'Vito',
+    lastName: 'Spatafore',
+    password: 'password'
+}
+const createAccountJohnny = {
+    id: 'johnny@soprano.it',
+    firstName: 'Johnny',
+    lastName: 'Sacramoni',
+    nickName: 'Sack',
+    password: 'password'
+}
+const createAccountPhil = {
+    id: 'phil@soprano.it',
+    firstName: 'Phil',
+    lastName: 'Leotardo',
+    password: 'password'
+}
+const createAccountLivia = {
+    id: 'mom@soprano.it',
+    firstName: 'Livia',
+    lastName: 'Soprano',
+    nickName: 'Mom',
+    password: 'password'
+}
+const createAccountSal = {
+    id: 'salvatore@soprano.it',
+    firstName: 'Salvatore',
+    lastName: 'Bonpensiero',
+    nickName: 'Big Pussy',
+    password: 'password'
+}
+const createAccountAngelo = {
+    id: 'angelo@soprano.it',
+    firstName: 'Angelo',
+    lastName: 'Garepi',
+    password: 'password'
+}
+const createAccountCarmine = {
+    id: 'carmine@soprano.it',
+    firstName: 'Carmine',
+    lastName: 'Lupertazzi Jr',
+    password: 'password'
+}
+const createAccountMikey = {
+    id: 'mikey@soprano.it',
+    firstName: 'CarMikeymine',
+    lastName: 'Palmice',
+    password: 'password'
+}
+const castOfSopranos=[createAccountCarmella, createAccountJunior, createAccountPaulie, 
+    createAccountBobby, createAccountVito, createAccountJohnny, 
+    createAccountPhil, createAccountLivia, createAccountSal,
+    createAccountCarmine, createAccountMikey, createAccountAngelo]
 synchroniseTony = {
     "flashDecks": [
         {
@@ -143,6 +224,9 @@ async function test() {
     //console.log("DELETING A DECK TONY");
     //let tonyDeleteDeck=await post(domain+'/synchronise', deleteDeckTony, tony.token);
     //console.log(tonyDeleteDeck);
+    for (var i in castOfSopranos) {
+        let createCastMembmer=await post(domain+'/account', castOfSopranos[i]);
+    }
     
 }
 
