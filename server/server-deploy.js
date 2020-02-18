dcf0d6cd550626aa0ff2a5ffd591060dc61fc99d
GiuseppeFitzsimmons <fitzsimmonsgiuseppe@gmail.com>
2020-02-18 21:36:09 +0100

    pipeline 3

--- server/server-deploy.js
+++ server/server-deploy.js
@@ -8,8 +8,8 @@ let deployParameters = '';
 let stackName;
 let local;
 let environment;
-let install='false';
-let deployToCloudFormation=true;
+let install = 'false';
+let deployToCloudFormation = true;
 process.argv.forEach(function (val, index, array) {
     if (val == '--profile') {
         profileArgument = '--profile ' + array[index + 1];
@@ -21,63 +21,67 @@ process.argv.forEach(function (val, index, array) {
         local = array[index + 1];
     } else if (val == '--env' || val == '--environment') {
         environment = array[index + 1];
-    } else if (val=='--install') {
-        install=array[index+1];
-    } else if (val=='-cf' || val=='--deployToCloudformation') {
-        if (array[index+1].toLowerCase()!=='true') {
-            deployToCloudFormation=false;
+    } else if (val == '--install') {
+        install = array[index + 1];
+    } else if (val == '-cf' || val == '--deployToCloudformation') {
+        if (array[index + 1].toLowerCase() !== 'true') {
+            deployToCloudFormation = false;
         }
     }
 });
 if (!deployParametersFile && environment) {
-    if (environment==='dev') {
-        deployParametersFile='deploy-parameters-dev.json';
-    } else if (environment==='prod') {
-        deployParametersFile='deploy-parameters-prod.json';
-    } else if (environment==='local' || local) {
-        deployParametersFile='deploy-parameters-local.json';
+    if (environment === 'dev') {
+        deployParametersFile = 'deploy-parameters-dev.json';
+    } else if (environment === 'prod') {
+        deployParametersFile = 'deploy-parameters-prod.json';
+    } else if (environment === 'local' || local) {
+        deployParametersFile = 'deploy-parameters-local.json';
     }
 }
 
-if (install!='false') {
-    if (process.platform==='darwin') {
-        installed=execSync('npm run install:linux --prefix commonlayer/nodejs');
+if (install != 'false') {
+    if (process.platform === 'darwin') {
+        installed = execSync('npm run install:linux --prefix commonlayer/nodejs');
     } else {
-        installed=execSync('npm run install:windows --prefix commonlayer/nodejs');
+        installed = execSync('npm run install:windows --prefix commonlayer/nodejs');
     }
     console.log(installed.toString());
-    installed=execSync('npm run install:all --prefix accountlambda');
+    installed = execSync('npm run install:all --prefix accountlambda');
     console.log("done installing accountlambda", installed.toString());
-    installed=execSync('npm run install:all --prefix adminlambda');
+    installed = execSync('npm run install:all --prefix adminlambda');
     console.log("done installing adminlambda", installed.toString());
-    installed=execSync('npm run install:all --prefix gallerylambda');
+    installed = execSync('npm run install:all --prefix gallerylambda');
     console.log("done installing gallerylambda", installed.toString());
-    installed=execSync('npm run install:all --prefix googleloginlambda');
+    installed = execSync('npm run install:all --prefix googleloginlambda');
     console.log("done installing googleloginlambda", installed.toString());
-    installed=execSync('npm run install:all --prefix polllambda');
+    installed = execSync('npm run install:all --prefix polllambda');
     console.log("done installing polllambda", installed.toString());
-    installed=execSync('npm run install:all --prefix rsvplambda');
+    installed = execSync('npm run install:all --prefix rsvplambda');
     console.log("done installing rsvplambda", installed.toString());
-    installed=execSync('npm run install:all --prefix synchroniselambda');
+    installed = execSync('npm run install:all --prefix synchroniselambda');
     console.log("done installing synchroniselambda", installed.toString());
 }
 if (!stackName && environment) {
-    if (environment==='dev') {
-        stackName='flashgang-dev';
-    } else if (environment==='prod') {
-        stackName='flashgang-prod';
+    if (environment === 'dev') {
+        stackName = 'flashgang-dev';
+    } else if (environment === 'prod') {
+        stackName = 'flashgang-prod';
     }
 }
 if (deployParametersFile) {
     let _json = JSON.parse(fs.readFileSync(deployParametersFile))
-    let _secrets = JSON.parse(fs.readFileSync('deploy-parameters-secrets.json'));
-
+    let _secrets = { Parameters: {} }
+    try {
+        _secrets = JSON.parse(fs.readFileSync('deploy-parameters-secrets.json'));
+    } catch (err) {
+        console.log(err)
+    }
     if (_json.Parameters) {
         _secrets = Object.assign({}, _secrets.Parameters, _json.Parameters);
     }
     deployParameters = Object.keys(_secrets).map(key => key + '=' + _secrets[key]).join(' ');
     deployParameters = `--parameter-overrides "PointlessParam=pointess ${deployParameters}"`;
-    if (environment==='dev' || environment==='prod') {
+    if (environment === 'dev' || environment === 'prod') {
         //TODO fix this - CrockStack wants quotes, AWS doesn't. According to AWS documentation, CrockStack is right.
         deployParameters = `--parameter-overrides PointlessParam=pointess ${deployParameters}`;
     }
@@ -89,18 +93,18 @@ if (deployParametersFile) {
 }
 if (local) {
     //killOldProccesses();
-    startDb(function(){
+    startDb(function () {
         console.log("DB started, starting server...");
         startServer(deployParameters);
     });
 } else {
     var packageCommand = `aws cloudformation package --template-file ${templateFile} --output-template-file packaged.yaml ${profileArgument} --s3-bucket wwdd-build-bucket-us-east-1`
     var deployCommand = `aws cloudformation deploy --template-file packaged.yaml --stack-name ${stackName}  ${profileArgument} --region us-east-1 --capabilities CAPABILITY_NAMED_IAM CAPABILITY_AUTO_EXPAND ${deployParameters}`
-    execSync(packageCommand, {stdio: 'inherit'});
+    execSync(packageCommand, { stdio: 'inherit' });
     console.log(packageCommand);
     if (deployToCloudFormation) {
-        execSync(deployCommand, {stdio: 'inherit', stderr: 'inherit'});
-        console.log(deployCommand);    
+        execSync(deployCommand, { stdio: 'inherit', stderr: 'inherit' });
+        console.log(deployCommand);
     } else {
         console.log("Package complete, not deploying to CloudFormation");
     }
@@ -108,7 +112,7 @@ if (local) {
 
 
 async function startDb(callback) {
-    await new Promise( (resolve, reject)=>{
+    await new Promise((resolve, reject) => {
         const child = spawn('java', ['-Djava.library.path=../../dynamoDB/DynamoDBLocal_lib', '-jar', '../../dynamoDB/DynamoDBLocal.jar', '-sharedDb']);
         child.on('exit', (code) => {
             console.log(`Child process exited with code ${code}`);
@@ -127,8 +131,8 @@ async function startServer(deployParameters) {
     if (process.platform === 'darwin') {
         crockCommand = '../node_modules/crockstack/cli.js';
     }
-    if (deployParameters.indexOf('--parameter-overrides ')==0) {
-        deployParameters=deployParameters.replace('--parameter-overrides ','');
+    if (deployParameters.indexOf('--parameter-overrides ') == 0) {
+        deployParameters = deployParameters.replace('--parameter-overrides ', '');
     }
-    execSync(crockCommand+' --parameter-overrides '+deployParameters, {stdio: 'inherit'})
+    execSync(crockCommand + ' --parameter-overrides ' + deployParameters, { stdio: 'inherit' })
 }
