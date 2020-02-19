9402d793d70064199b2f80e0d218c67b3f1cafbd
giuseppefitzsimmons <giuseppefitzsimmons@gmail.com>
2020-02-19 15:53:13 +0100

    Uncommented sendmail

--- server/synchroniselambda/index.js
+++ server/synchroniselambda/index.js
@@ -49,7 +49,7 @@ exports.handler = async (event, context) => {
                             if (user.firstName || user.lastName) {
                                 invitor = `${user.firstName ? user.firstName : ''} ${user.lastName ? user.lastName : ''}`
                             }
-                            //await mailUtility.sendInvitationMail(invitor, member.email, flashGang.name)
+                            await mailUtility.sendInvitationMail(invitor, member.email, flashGang.name)
                             member.state = 'INVITED'
                             member.invitedBy = token.sub;
                             console.log("INVITEDBY BUG SAVING MEMBER", member);
